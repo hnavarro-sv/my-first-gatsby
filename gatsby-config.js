019f5ccee4b0e1626f0a1c4f2062072bf563bc1f
Hugo Navarro <hugo.navarro@mh.gob.sv>
2021-07-15 08:11:10 -0600

    My first Gatsby App

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,42 +1,7 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
-
 module.exports = {
-  plugins: [
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages',
-      },
-    },
-    {
-      resolve: 'gatsby-source-cosmicjs',
-      options: {
-        bucketSlug: process.env.COSMIC_BUCKET,
-        objectTypes: ['posts','settings'],
-        apiAccess: {
-          read_key: process.env.COSMIC_READ_KEY,
-        },
-        localMedia: true
-      }
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-typography',
-      options: {
-        pathToConfigModule: 'src/utils/typography',
-      },
-    },
-  ],
-}
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "My Gatsby Site",
+  },
+  plugins: ["gatsby-plugin-gatsby-cloud"],
+};
